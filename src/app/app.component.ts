eea9ea43130a9b664c11908f4652057e0f584190
Bharath Ravi <bharathravi27@gmail.com>
2024-04-18 22:09:46 +0530

    sic componeent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { fileUploadSize2mbProviderService } from './file-upload-size-provider-2mb.directive';
+import { fileUploadSize10mbProviderService } from './file-upload-size-provider-5mb.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FileUploadComponent,
+    fileUploadSize2mbProviderService,
+    fileUploadSize10mbProviderService,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'injectors-ftw';
